13d0f0c5903a5a6482d68aa3bda38cf3e124e101
hpawig <pawighannah@gmail.com>
2024-07-26 11:56:21 -0700

    adding mst/oc function to method script

--- scripts/strict-methods.R
+++ scripts/strict-methods.R
@@ -86,8 +86,74 @@ test_coverage <- function(a,b, conf.level) {
 ##               Modified Stern/Optimal Coverage               ##
 ##-------------------------------------------------------------##
 
-
-
+# K = largest number of x to create a CI for.
+# this function returns a data frame of all CIs for x = 0 to x = K
+OC <- function(K, conf.level = 0.95) {
+  x <- c(0:K)
+  
+  # initialize necessary vectors
+  lower <- c()
+  upper <- c()
+  
+  # step 1  
+  a <- 0 # starting a
+  b <- 0 # starting b
+  lower[1] <- 0
+  
+  
+  
+  while (a < (K+1)) {
+    # step 2
+    
+    # this creates the current curve's function so we can find its root.    
+    f <- function(lambda) { 
+      if (b != 0) {
+        return((ppois(b, lambda) - ppois(a-1, lambda)) - 0.95)
+      } else {
+        return(ppois(b, lambda) - 0.95)
+      }
+    }  
+    
+    
+    if((test_coverage(a+1, b+1, conf.level) != T)) {
+      
+      
+      # this loop finds interval (start,end) to search for current a-b's root
+      start <- AC_max_coords(a, b)$lambda # start searching at current a-b's maximum lambda
+      a0 <- (a+1); b0 <- (b+1)
+      while (sum(dpois(a0:b0,
+                       AC_max_coords(a0, b0)$lambda)) >= conf.level) {
+        a0 <- a0+1 
+        b0 <- b0+1
+      } 
+      end <- AC_max_coords(a0, b0)$lambda # end search at next a-b's maximum lambda
+      
+      
+      # there's an "extra" +1 for indexing purposes
+      lower[(b+1)+1] <- uniroot(f, c(start, end))$root # lower limit for (b+1), aka new b
+      b <- b + 1
+      
+      
+    } else {
+      # step 3
+      
+      a <- a + 1
+      b <- b + 1
+      
+      # setting coincidental endpoint (by prop 2.2c)
+      lower[b+1] <- AC_max_coords(a, b)$lambda # current b's lower bound
+      upper[a] <- lower[b+1]              # a-1's lower bound
+      
+    }
+    
+  }
+  lower <- lower[1:(K+1)]
+  upper <- upper[1:(K+1)]
+  x <- x[1:(K+1)] # ensuring all vectors are equal in length. 
+  # b/c extra lower endpoints will be cut off
+  
+  return(data.frame(x, lower, upper))
+}
 
 
 
