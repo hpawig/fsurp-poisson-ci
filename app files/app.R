c8a4ceddb9d5fda56343afc561910d93505c3a36
hpawig <pawighannah@gmail.com>
2024-08-23 20:29:03 -0700

    added note to user about x-inputs in sidebar panel; added card displaying user's inputs in main panel; the 8 methods have functionality in the app.

--- app files/app.R
+++ app files/app.R
@@ -21,8 +21,9 @@ source("server-fns.R", encoding = "UTF-8")
 
 
 # Define UI for application that takes in user's observed x and confidence level
-
+options(warn=-1) # turn warnings off temporarily
 ui <- page_sidebar(
+   
   title = "Poisson Confidence Interval Generator",
   sidebar = sidebar(
     width = 450,
@@ -34,7 +35,7 @@ ui <- page_sidebar(
         label = "Select a Confidence Procedure",
         choices = list("Wald" = 1, "Rao's Score" = 2, "Wilks' Likelihood Ratio" = 3, 
                        "Analog to Clopper-Pearson" = 4, "Modified Stern/Optimal Coverage" = 5,
-                       "Crow & Gardner" = 6, "Blaker" = 7)
+                       "Crow & Gardner" = 6, "Blaker" = 7, "Conditional Minimal Cardinality" = 8)
       ),
       
       # slider to choose confidence level
@@ -46,11 +47,24 @@ ui <- page_sidebar(
       
       # user enters their observed x
       numericInput(inputId = "obs_x", 
-                   label = "Enter observed x", value = 10),
+                   label = "Enter observed x", value = 10,
+                   min = 0),
+      
+      card_footer(
+        div(tags$small("*Note: " , 
+                       tags$i("x"), " must be an integer representing the number of ",
+                       tags$i("total"), " events over time period of interest"),
+            align = "center", style = "color: red")
+      )
+      
+      ),
+    
 
+      card(
+      card_header("Options"),
       
       # user choice of intervals to be displayed
-      checkboxInput(inputId = "checkbox",
+      checkboxInput(inputId = "displayAll",
                     label = "Display intervals up to observed x",
                     value = FALSE),
       selectInput(
@@ -58,13 +72,14 @@ ui <- page_sidebar(
         label = "Choose up to 4 decimal places",
         choices = list("2" = 1, "3" = 2, "4" = 3)
       ),
+        
       
       # button to submit user's changes
       submitButton("Submit", icon("refresh")),
       
       # contact information section
       card_footer(
-        div("Shiny App & Base R code by" , 
+        div("Shiny App by" , 
             a(href = "https://www.linkedin.com/in/hannahpawig/", target = "_blank", "Hannah Pawig"),
             align = "right", style = "font-size: 8pt"),
         div("Maintenance by" , 
@@ -79,10 +94,16 @@ ui <- page_sidebar(
   ),
   
   # main panel elements
+  card (
+    height = 160,
+    card_header("Inputs"),
+    verbatimTextOutput(outputId = "inputs")
+  ),
   
   card(
-    full_screen = TRUE,
-    card_header("Confidence Interval for x"),
+    full_screen = T,
+    card_header("Confidence Intervals"),
+    max_height = 440,
     tableOutput(outputId = "intervals")
    )
 )
@@ -95,17 +116,27 @@ ui <- page_sidebar(
   digits <- reactive({
     as.numeric(input$digits) + 1
     })
-  
 
 
   # output table of confidence interval(s)
     output$intervals <- renderTable({
+      
       expr = find_ci(method = input$method, x = input$obs_x,
-                     conf.level = input$conf_level, all = input$checkbox,
-                     digits = input$digits)
+                     conf.level = input$conf_level, all = input$displayAll,
+                     digits = input$digits) |> select(x:interval)
     }, digits = digits)
     
 
-}
+  # output string of user's inputs
+    output$inputs <- renderText({
+      find_ci(method = input$method, x = input$obs_x,
+              conf.level = input$conf_level, all = input$displayAll,
+              digits = input$digits)$input.str[1]
+    })
+
+ }
+ options(warn=0) # turn warnings back on
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+
--- app files/server-fns.R
+++ app files/server-fns.R
@@ -14,34 +14,60 @@ library(tidyverse)
 # for the user's observed x
 
 find_ci <- function(method, x, conf.level, all, digits) {
+  obs.x <- x
+  
   ci <- c()
   conf.level <- conf.level/100
   digits <- as.numeric(digits) + 1
   if (method == "1") { # Wald
-    ci <- wald_CI(x, conf.level, all)
+    ci <- Wald.pois(x, conf.level, all)
+    method.str <- "Wald (1812)"
+    
   } else if (method == "2") { # Rao's Score
-    ci <- rao_score_CI(x, conf.level, all)
+    ci <- RaoScore.pois(x, conf.level, all)
+    method.str <- "Rao's Score (1948)"
+    
   } else if (method == "3") { #Wilks Likelihood Ratio
-    ci <- wilksLR_CI(x, conf.level, all, digits)
+    ci <- WilksLR.pois(x, conf.level, all, digits)
+    method.str <- "Wilks' Likelihood Ratio (1938)"
+    
   } else if (method == "4") { # Clopper Pearson
-    ci <- clopperPearson_CI(x, conf.level, all)
+    ci <- ClopperPearson.pois(x, conf.level, all)
+    method.str <- "Clopper-Pearson (1934)"
+    
   } else if (method == "5") { # MST/OC
-    ci <- OC(x, conf.level, all)
+    ci <- OC.pois(x, conf.level, all)
+    method.str <- "Modified Stern/Optimal Coverage (2014)"
+    
   } else if (method == "6") { # CG
-    ci <- CG(x, conf.level, all)
+    ci <- CG.pois(x, conf.level, all)
+    method.str <- "Crow & Gardner (1959)"
+    
   } else if (method == "7") { # Blaker's
-    ci <- blaker_CI(x, conf.level, digits)
+    ci <- Blaker.pois(x, conf.level)
+    method.str <- "Blaker (2000)"
+    
   } else if (method == "8") { # CMC
-    ci <- CMC_CI(x, conf.level, all)
+    ci <- CMC.pois(x, conf.level, all)
+    method.str <- "Conditional Minimal Cardinality (2023)"
+    
   }
+    
   
+  # adding 2 new columns
+  # interval: lower and upper bounds in the form of an open interval
+  # input.str: contains the string that the user will see, displaying their inputs
+  
+  ci <- ci |> 
+    mutate(interval = paste0("(",round(lower, digits),", ",round(upper,digits),")"),
+           input.str = paste0("method: ", method.str, "\nx-input: ", obs.x,
+                              "\nconfidence level: ", 100*conf.level, "%\n"))
 
+  
+  
   return(ci) # ci is a data.frame of CIs from x = 0 to x = observed when all = TRUE
              # ci is a data.frame of 1 row for CI of observed x ONLY when all = FALSE
 }
-
-
-
 
 
 
